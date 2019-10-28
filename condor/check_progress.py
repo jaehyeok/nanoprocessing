429e5b79a151ca6311a80a7107148cb22caa0375
Jae Hyeok Yoo <jaehyeokyoo@gmail.com>
2019-10-28 21:25:43 +0900

    print table of processing status

--- condor/check_progress.py
+++ condor/check_progress.py
@@ -2,8 +2,35 @@
 import sys
 import os
 import os.path
+import glob
 
-outputdir="/xrootd_user/jaehyeok/xrootd/2016v4/2019_10_15/processed/"
+year=sys.argv[1]
+
+outputdir="/xrootd_user/jaehyeok/xrootd/2016v4/2019_10_23/"
+if year == "2017":
+	outputdir="/xrootd_user/jaehyeok/xrootd/2017v4/2019_10_23/"
+if year == "2018":
+	outputdir="/xrootd_user/jaehyeok/xrootd/2018v4/2019_10_23/"
+
+flistdir="/cms/ldap_home/jaehyeok/flist/"+year
+print('------------------------------------------------------------------------------------------------')
+print('%50s %10s %15s %15s' %("tag", "flist", "processed", "completion"))
+print('------------------------------------------------------------------------------------------------')
+
+# compare flist and the output directory, and generate a status summary table
+flists = os.listdir(flistdir)
+for flist in flists:
+#flist_WW_TuneCP5.txt
+	tag=flist.replace('.txt','').replace('flist_','') # extract tag
+	if "outputdir" in tag:
+		continue
+	num_lines = sum(1 for line in open(flistdir+"/"+flist)) # number of files in flist
+	num_processed = sum(1 for line in glob.glob(outputdir+"/*"+tag+"*")) # number of files processed
+	print('%50s %10d %15d %15.1f%%' %(tag, num_lines, num_processed, 1.0*num_processed/num_lines*100))
+print('------------------------------------------------------------------------------------------------')
+
+
+# generate list of files that have been processed
 splits = outputdir.split("/")
 list_output_file = open("/cms/ldap_home/jaehyeok/flist/flist_outputdir_"+splits[4]+"_"+splits[5]+".txt", "w")
 
