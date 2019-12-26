cf02da43760120481a383bba0a078465284a61dd
YongHo, Jeong <yonghojeong57@gmail.com>
2019-12-26 18:49:46 +0900

    Add files via upload

--- MergeFile.py
+++ MergeFile.py
@@ -5,38 +5,48 @@ from ROOT import TChain, TSelector, TTree, TH1F, TCanvas, TPad, TStyle, TString
 year=sys.argv[1]
 
 simple_in = "/cms/ldap_home/jaehyeok/nanoprocessing/condor/xrootd/"
-simple_out = "/cms/scratch/yjeong/CMSSW_7_1_0/src/nanoprocessing/mc/"
-#list_dir = "/cms/ldap_home/yjeong/flist/"
-list_dir = "/cms/ldap_home/jaehyeok/flist/"
+simple_out = "/cms/scratch/yjeong/CMSSW_7_1_0/src/nanoprocessing/mc_merged/"
 
 inputdir = simple_in+"2016v4/2019_12_10/skim_rpvfitnbge0/"
-outputdir = simple_out+"2016/"
-flists = os.listdir(list_dir+"2016/")
+outputdir = simple_out+"2016_1/"
+flists = os.listdir(inputdir)
 
 if year == "2017":
 	inputdir = simple_in+"2017v4/2019_10_23/skim_rpvfit/"
 	outputdir = simple_out+"2017/"
-	flists = os.listdir(list_dir+"2017/")
+	flists = os.listdir(inputdir)
 
 if year == "2018":
 	inputdir = simple_in+"2018v4/2019_10_23/skim_rpvfit/"
 	outputdir = simple_out+"2018/"
-	flists = os.listdir(list_dir+"2018/")
+	flists = os.listdir(inputdir)
+
+process=[]
+
+for i in range(0,len(flists)-1):
+	tag=flists[i].replace('_rpvfitnbge0','').replace('.root','').split('fatjetbaby_')
+	process.append(tag[1])
+
+process = list(set(process))
 
 print('---------------------------------------------------'+year+'-------------------------------------------------------')
-print('%40s %20s %25s %20s' %("tag", "Merged files", "Not merged event", 'Merged event'))
+print('%40s %15s %20s %15s %15s' %("tag", "Merged files", "Not merged event", 'Merged event', 'Event ratio'))
 print('--------------------------------------------------------------------------------------------------------------')
 
-for flist in flists:
-	tag = flist.replace('flist_','').replace('.txt','')
+for flist in process:
 	mc = TChain("tree")
-	mc.Add(inputdir+"*_fatjetbaby_"+tag+"_rpvfitnbge0"+".root")
-	nfiles=mc.Add(inputdir+"*_fatjetbaby_"+tag+"_rpvfitnbge0"+".root")
+	mc.Add(inputdir+"*_fatjetbaby_"+flist+"_rpvfitnbge0"+".root")
+	nfiles=mc.Add(inputdir+"*_fatjetbaby_"+flist+"_rpvfitnbge0"+".root")
 	nfiles=str(nfiles)
 	t1 = mc.GetEntries()
-	t1 = str(t1)
-	mc.Merge(outputdir+tag+"_"+nfiles+".root")
+	mc.Merge(outputdir+flist+"_"+nfiles+".root")
 	t2 = mc.GetEntries()
+	if t1==0 or t2==0:
+		t3 = 0
+	else:
+		t3 = t1/t2
+	t1 = str(t1)
 	t2 = str(t2)
-	print('%40s %20s %25s %20s' %(tag, nfiles, t1, t2))
+	t3 = str(t3)
+	print('%40s %15s %20s %15s %15s' %(flist, nfiles, t1, t2, t3))
 print('--------------------------------------------------------------------------------------------------------------')
