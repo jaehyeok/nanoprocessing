4cd9b3ed7d5cabf25fa4f2511f56b8ddb11f32de
YongHo, Jeong <yonghojeong57@gmail.com>
2020-01-31 16:20:08 +0900

    include NanoAOD version 5

--- condor/submit_all.py
+++ condor/submit_all.py
@@ -6,15 +6,22 @@ import os.path
 year=sys.argv[1]
 
 #outputdir="/xrootd_user/jaehyeok/xrootd/2017v4/2019_09_09"
-outputdir="root://cms-xrdr.private.lo:2094//xrd/store/user/jaehyeok/2016v4/2019_11_07/"
-samplelist="samples/samples2016.txt"
-inputmctag="RunIISummer16NanoAODv4"
+#outputdir="root://cms-xrdr.private.lo:2094//xrd/store/user/jaehyeok/2016v4/2019_10_23/"
+outputdir="root://cms-xrdr.private.lo:2094//xrd/store/user/yjeong/nanoprocessing/2016/"
+#outputdir="/xrootd_user/yjeong/xrootd/nanoprocessing/2016"
+#samplelist="samples/samples2016.txt"
+#inputmctag="RunIISummer16NanoAODv4"
+samplelist="samples/samples2016_NanoAODv5.txt"
+inputmctag="RunIISummer16NanoAODv5"
+
 if year == "2017":
-	outputdir="root://cms-xrdr.private.lo:2094//xrd/store/user/jaehyeok/2017v4/2019_10_23/"
+	outputdir="root://cms-xrdr.private.lo:2094//xrd/store/user/yjeong/nanoprocessing/2017/"
+	#outputdir="/xrootd_user/yjeong/xrootd/nanoprocessing/2017"
 	samplelist="samples/samples2017.txt"
 	inputmctag="RunIIFall17NanoAODv4"
 if year == "2018":
-	outputdir="root://cms-xrdr.private.lo:2094//xrd/store/user/jaehyeok/2018v4/2019_10_23/"
+	outputdir="root://cms-xrdr.private.lo:2094//xrd/store/user/yjeong/nanoprocessing/2018/"
+	#outputdir="/xrootd_user/yjeong/xrootd/nanoprocessing/2018"
 	samplelist="samples/samples2018.txt"
 	inputmctag="RunIIAutumn18NanoAODv4"
 
@@ -26,13 +33,11 @@ print " sample list: "+samplelist
 print "------------------------------------------------------------------------------------------------------------"
 print ""
 
-# make a directory for submission scripts
-os.system("mkdir submit_scripts")
-
 # open sample list file to write
 f = open(samplelist, 'r')
 
 splits = outputdir.split("/")
+#list_processed = "flist_"+splits[4]+"_"+splits[5]+".txt"
 list_processed = "flist_outputdir_"+splits[8]+"_"+splits[9]+".txt"
 
 # make file list and condor submisstion script
@@ -45,16 +50,16 @@ for line in lines:
   if "13TeV" in splits[0]:
     f_list = os.listdir("/xrootd/store/mc/"+inputmctag+"/"+splits[0]+"")
     process=line.split('_13TeV')[0]
-    list_file = open("/cms/ldap_home/jaehyeok/flist/"+year+"/flist_"+process+".txt", "w")
+    list_file = open("/cms/ldap_home/yjeong/flist/"+year+"/flist_"+process+".txt", "w")
     for subdir in f_list:
       f_list_subdir = os.listdir("/xrootd/store/mc/"+inputmctag+"/"+splits[0]+"/"+subdir)
       for files in f_list_subdir:
         filename="/xrootd/store/mc/"+inputmctag+"/"+splits[0]+"/"+subdir+"/"+files+"\n"
         if "BB833C07-5ECA-5E43-8A99-38083AACE497" not in filename:
           list_file.write(filename)
-    condor_file = open("submit_scripts/condor_"+process+"_"+year+".jds", "w")
+    condor_file = open("submit_scripts/"+year+"/condor_"+process+".jds", "w")
     condor_file.write("executable              = run.sh\n");
-    condor_file.write("arguments               = /xrootd/store/mc/"+inputmctag+"/"+splits[0]+" "+outputdir+" "+process+" " +list_processed+"\n");
+    condor_file.write("arguments               = "+inputmctag+"/"+splits[0]+" "+outputdir+" "+process+" " +list_processed+"\n");
     condor_file.write("transfer_input_files    = process_nano.exe flist_"+process+".txt\n");
     condor_file.write("log                     = log/run_"+process+".$(Cluster).$(Process).log\n");
     condor_file.write("output                  = log/run_"+process+"_output.$(Cluster).$(Process).out\n");
@@ -63,19 +68,19 @@ for line in lines:
     condor_file.write("#when_to_transfer_output = ON_EXIT\n");
     condor_file.write("queue");
     condor_file.close()
-    print("condor_submit submit_scripts/condor_"+process+"_"+year+".jds")
+    print("condor_submit submit_scripts/condor_"+process+".jds")
     #os.system("condor_submit condor_"+process+".jds")
   # data
   elif "Run201" in splits[0]:
     f_list = os.listdir(splits[0])
     process=line.split('/')[5]+line.split('/')[4]
-    list_file = open("/cms/ldap_home/jaehyeok/flist/"+year+"/flist_"+process+".txt", "w")
+    list_file = open("/cms/ldap_home/yjeong/flist/"+year+"/flist_"+process+".txt", "w")
     for subdir in f_list:
       f_list_subdir = os.listdir(splits[0]+"/"+subdir)
       for files in f_list_subdir:
         filename=splits[0]+"/"+subdir+"/"+files+"\n"
         list_file.write(filename)
-    condor_file = open("submit_scripts/condor_"+process+".jds", "w")
+    condor_file = open("submit_scripts/"+year+"/condor_"+process+".jds", "w")
     condor_file.write("executable              = run.sh\n");
     condor_file.write("arguments               = "+splits[0]+" "+outputdir+" "+process+" "+list_processed+"\n");
     condor_file.write("transfer_input_files    = process_nano.exe flist_"+process+".txt\n");
