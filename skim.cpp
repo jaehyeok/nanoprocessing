4a526bea029c4b770d7f5ba77d6d0425192b0cd1
yjeong <yonghojeong57@gmail.com>
2020-05-18 12:28:55 +0900

    remove the SingleMuon data in 2017 and 2018, && change the directory for skim.

--- skim.cpp
+++ skim.cpp
@@ -124,7 +124,7 @@ int main(int argc, char **argv)
     skim         = argv[2];
    
     outputdir = inputdir;
-    outputdir.ReplaceAll("processed_sys_v1", Form("skim_%s_sys_v1-1", skim.Data()));
+    outputdir.ReplaceAll("processed", Form("skim_%s", skim.Data()));
     cout << " input   dir  : " << inputdir << endl;
     cout << " output  dir  : " << outputdir << endl;
     cout << " skim         : " << skim << endl;
@@ -135,11 +135,11 @@ int main(int argc, char **argv)
 
   // get list of files in a directory
   //vector<TString> files = globVector(Form("%s/*.root", inputdir.Data())); 
-  //vector<TString> files = globVector(Form("%s/*_QCD*.root", inputdir.Data())); 
+  vector<TString> files = globVector(Form("%s/*_QCD*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_DYJets*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_TT*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_W*.root", inputdir.Data())); 
-  vector<TString> files = globVector(Form("%s/*_ST*.root", inputdir.Data())); 
+  //vector<TString> files = globVector(Form("%s/*_ST*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_SMS*.root", inputdir.Data())); 
 
 	cout << "skimming " << files.size() << " files" << endl;
