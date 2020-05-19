12a5e5f123ea9bed3ef35f6ed230d12d0664488b
yjeong <yonghojeong57@gmail.com>
2020-05-19 10:57:16 +0900

    modified skim cut

--- skim.cpp
+++ skim.cpp
@@ -71,9 +71,9 @@ void skimonefile(TString inputfile, TString outputdir, TString skim)
 		skimcut="nbm>0&&mj12>500&&njets>=4";
 	}
 	else if(skim=="rpvfitnbge0") {
-		if(inputfile.Contains("JetHTRun")) skimcut="(sys_ht[0]>1200 || sys_ht[1]>1200 || ht>1200) && (sys_mj12[0]>500 || sys_mj12[1]>500 || mj12>500) && (sys_njets[0]>=4 || sys_njets[1]>=4 || njets>=4) && stitch_ht==1";
-		else if(inputfile.Contains("TTJets_Tune")) skimcut="(sys_ht[0]>1200 || sys_ht[1]>1200 || ht>1200) && (sys_mj12[0]>500 || sys_mj12[1]>500 || mj12>500) && (sys_njets[0]>=4 || sys_njets[1]>=4 || njets>=4) && stitch_ht==1 && nbm>=2";
-		else skimcut="(sys_ht[0]>1200 || sys_ht[1]>1200 || ht>1200) && (sys_mj12[0]>500 || sys_mj12[1]>500 || mj12>500) && (sys_njets[0]>=4 || sys_njets[1]>=4 || njets>=4) && nbm>=2";
+		if(inputfile.Contains("JetHTRun")) skimcut = "nbm<2";
+		else if(inputfile.Contains("TTJets_Tune")) skimcut="(sys_ht[0]>1200 || sys_ht[1]>1200 || ht>1200) && (sys_mj12[0]>500 || sys_mj12[1]>500 || mj12>500) && (sys_njets[0]>=4 || sys_njets[1]>=4 || njets>=4) && stitch_ht==1";
+		else skimcut="(sys_ht[0]>1200 || sys_ht[1]>1200 || ht>1200) && (sys_mj12[0]>500 || sys_mj12[1]>500 || mj12>500) && (sys_njets[0]>=4 || sys_njets[1]>=4 || njets>=4)";
 	}
 	else if(skim=="ht1000") {
 		skimcut="ht>1000";
@@ -134,14 +134,14 @@ int main(int argc, char **argv)
   gSystem->mkdir(outputdir.Data());
 
   // get list of files in a directory
-  //vector<TString> files = globVector(Form("%s/*.root", inputdir.Data())); 
+  vector<TString> files = globVector(Form("%s/*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_QCD*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_DYJets*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_TT*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_W*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_ST*.root", inputdir.Data())); 
   //vector<TString> files = globVector(Form("%s/*_SMS*.root", inputdir.Data())); 
-  vector<TString> files = globVector(Form("%s/*JetHTRun*.root", inputdir.Data())); 
+  //vector<TString> files = globVector(Form("%s/*JetHTRun*.root", inputdir.Data())); 
 
 	cout << "skimming " << files.size() << " files" << endl;
 	
