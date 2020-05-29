e285cc6a02cedae0c6c0f5676e823a00136b90ab
yjeong <yonghojeong57@gmail.com>
2020-05-29 15:30:09 +0900

    include the script for normalization btag weight

---
+++ scripts/run_norm_weight.sh
@@ -0,0 +1,1 @@
+./norm_weights.exe /xrootd_user/yjeong/xrootd/nanoprocessing/2017/merged_rpvfirnbeg0_sys_v1-1/ 1600 /cms/scratch/yjeong/norm_test /xrootd_user/yjeong/xrootd/nanoprocessing/2017/processed/
