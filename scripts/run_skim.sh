89be68c653e496b935de326be3522bd1ab8e11f5
yjeong <yonghojeong57@gmail.com>
2020-05-29 10:37:17 +0900

    remove renormalization.cpp

--- scripts/run_skim.sh
+++ scripts/run_skim.sh
@@ -1,3 +1,3 @@
 for x in {0,1,2,3,4};
-do ./skim.exe /xrootd_user/yjeong/xrootd/nanoprocessing/2017/processed/ rpvfitnbge0_test $x > skim_log_$x.txt &
+do ./skim.exe /xrootd_user/yjeong/xrootd/nanoprocessing/2017/processed rpvfitnbge0_test $x > skim_log_$x.txt &
 done
