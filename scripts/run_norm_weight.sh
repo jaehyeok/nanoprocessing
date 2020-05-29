152fac899c22752a229edf1fc24d3ef934fe3d62
yjeong <yonghojeong57@gmail.com>
2020-05-29 18:54:44 +0900

    update the script

--- scripts/run_norm_weight.sh
+++ scripts/run_norm_weight.sh
@@ -1,4 +1,4 @@
 for tag in {SMS-T1tbs_RPV_mGluino,DYJetsToLL_M-50,QCD_HT,ST_,TT,WJets,WW,WZ,ZZ};
 do echo ${tag}
-./norm_weights.exe /xrootd_user/yjeong/xrootd/nanoprocessing/2017/merged_rpvfitnbge0/ ${tag} /xrootd_user/yjeong/xrootd/nanoprocessing/2017/merged_norm /xrootd_user/yjeong/xrootd/nanoprocessing/2017/processed > norm_log.txt &
+./norm_weights.exe /xrootd_user/yjeong/xrootd/nanoprocessing/2017/merged_rpvfitnbge0/ ${tag} /xrootd_user/yjeong/xrootd/nanoprocessing/2017/merged_norm /xrootd_user/yjeong/xrootd/nanoprocessing/2017/processed > norm_log_${tag}.txt &
 done
