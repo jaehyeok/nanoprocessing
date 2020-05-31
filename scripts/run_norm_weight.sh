1f0c5038c64a91fc638be4937ca063a0e6c1c12e
yjeong <yonghojeong57@gmail.com>
2020-05-31 16:21:26 +0900

    remove the 0 event in 2016 samples

--- scripts/run_norm_weight.sh
+++ scripts/run_norm_weight.sh
@@ -1,8 +1,6 @@
-#for tag in {SMS-T1tbs_RPV_mGluino,QCD_HT,ST_,DYJets,WJets,WW,WZ,ZZ,TTJets,TTZ};
-for tag in {SMS-T1tbs_RPV_mGluino,QCD_HT,ST_};
-#for tag in {DYJetsToLL_M-50_HT-1200to2500,DYJetsToLL_M-50_HT-2500toInf,DYJetsToLL_M-50_HT-400to600,DYJetsToLL_M-50_HT-600to800,DYJetsToLL_M-50_HT-800to1200};
-#for tag in {WJets,WW,WZ,ZZ};
-#for tag in {TTW,TTZ};
+#for tag in {SMS-T1tbs_RPV_mGluino,QCD,ST_,DYJets,WJets,WW,WZ,ZZ,TTJets,TTZ,TTTT};
+#for tag in {SMS-T1tbs_RPV_mGluino,QCD,ST_,DYJets};
+for tag in {WJets,TTTT};
 do echo ${tag}
-./norm_weights.exe /xrootd_user/yjeong/xrootd/nanoprocessing/2018/merged_rpvfitnbge0/ ${tag} /xrootd_user/yjeong/xrootd/nanoprocessing/2018/merged_norm/ /xrootd_user/yjeong/xrootd/nanoprocessing/2018/processed/ > norm_log_${tag}.txt &
+./norm_weights.exe /xrootd_user/yjeong/xrootd/nanoprocessing/2018/merged_rpvfitnbge0/ ${tag} /xrootd_user/yjeong/xrootd/nanoprocessing/2018/merged_norm/ /xrootd_user/yjeong/xrootd/nanoprocessing/2018/processed/ > log_norm_${tag}.txt &
 done
