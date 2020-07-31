4839dc328d5bd5faee48fc64ac5d5adbf4876301
yjeong <yonghojeong57@gmail.com>
2020-08-01 02:49:18 +0900

    edit the skim script

--- scripts/run_skim.sh
+++ scripts/run_skim.sh
@@ -1,3 +1,3 @@
 for x in {0,1,2,3,4,5,6,7,8,9};
-do ./skim.exe /xrootd_user/yjeong/xrootd/nanoprocessing/2016/processed rpvfitnbge0 $x > log_skim_$x.txt &
+do ./skim.exe /xrootd_user/yjeong/xrootd/nanoprocessing/$1/processed rpvfitnbge0 $x > log_skim_$x.txt &
 done
