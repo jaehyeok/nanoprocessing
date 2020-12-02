881c973f2ce7bbc94e970e34a343d3ad8326166e
YongHo Jeong <yongho.jeong@cern.ch>
2020-12-03 02:25:38 +0900

    validation for applied HEM effect

--- process_nano.cpp
+++ process_nano.cpp
@@ -893,20 +893,21 @@ void process_nano(TString inputfile, TString outputdir, float sumWeights, TStrin
     bool hem_tf;
     for(int iJ = 0; iJ < nJet; iJ++) 
     {
+      hem_tf = false;
       if(isData){//HEM effect applied in data
-        if(run<319077) hem_tf=false;
-        else if(run>=319077 && Jet_eta[iJ]>-3.0 && Jet_eta[iJ]<-1.3 && Jet_phi[iJ]>-1.57 && Jet_phi[iJ]<-0.87) hem_tf=true;
+        if(run>=319077 && Jet_eta[iJ]>-3.0 && Jet_eta[iJ]<-1.3 && Jet_phi[iJ]>-1.57 && Jet_phi[iJ]<-0.87){
+ 	  hem_tf=true;
+	  cout<<"run>=319077: "<<"HEM issue in data: "<< hem_tf <<endl;
+	}
+	cout<<"run<319077: "<<"HEM issue in data: "<< hem_tf <<endl;
       }
 
       if(!isData){//HEM effect applied in MC
-        if(event%10 >= 0 && event%10 <= 5){
-  	  hem_tf = true;
-	  //cout<<"event selector: "<< event%10 <<", hem_tf_MC_0~5: "<<hem_tf<<endl;
+        if((event%10>=0 && event%10<=5) && Jet_eta[iJ]>-3.0 && Jet_eta[iJ]<-1.3 && Jet_phi[iJ]>-1.57 && Jet_phi[iJ]<-0.87){
+	  hem_tf = true;
+	  cout<<"event selector: "<<event%10<<", HEM issue in MC: "<< hem_tf <<endl;
 	}
-        else{
-          hem_tf = false;
-	  //cout<<"event selector:" << event%10 <<", hem_tf_MC_6~9: "<<hem_tf<<endl;
-        }
+        cout<<"event selector: "<<event%10<<", hem_tf: "<< hem_tf <<endl;
       }
 
       jets_pt.push_back(Jet_pt[iJ]); 
@@ -952,8 +953,10 @@ void process_nano(TString inputfile, TString outputdir, float sumWeights, TStrin
       if(abs(Jet_eta[iJ])>2.4) continue;
       if(!jetid)               continue; 
       if(jetislep)             continue; 
-      if(year==2018 && jets_hem.at(iJ)) continue;
-     
+      if(year == 2018){
+        if(jets_hem.at(iJ)) continue;
+      }
+
       // deepCSV  cuts
       float csv_cut = 0.6321; 
       if(year==2017) csv_cut = 0.4941;
@@ -1043,7 +1046,9 @@ void process_nano(TString inputfile, TString outputdir, float sumWeights, TStrin
       if(jets_pt.at(iJ)<30)           continue;
       if(abs(jets_eta.at(iJ))>2.4)    continue;
       if(jets_id.at(iJ)==false)       continue;
-      if(year==2018 && jets_hem.at(iJ)) continue;
+      if(year==2018){
+        if(jets_hem.at(iJ)) continue;
+      }
 
       input_particles.push_back(fastjet::PseudoJet(JetLV.Px(), JetLV.Py(), JetLV.Pz(), JetLV.E()));
       h2->Fill(JetLV.Eta(), JetLV.Phi(), JetLV.E());
