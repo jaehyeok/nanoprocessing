0ff39a632f9d9957937e04d7d0bcc0f6ecda5a79
bjhong <bjhong96@gmail.com>
2023-08-14 16:06:48 +0900

    slurm script for getting pileup weight of each year

---
+++ scripts/pu_weight_UL2017.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH -J PU_WEIGHT_UL2017
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -o /home/bjhong/nanoprocessing/out_%x.o%j.txt
+#SBATCH -e /home/bjhong/nanoprocessing/err_%x.e%j.txt
+cd /home/bjhong/nanoprocessing
+echo $PWD
+srun ./get_puweights.sh UL2017
